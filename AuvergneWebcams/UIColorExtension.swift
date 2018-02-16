9b1bee0eedfa7e545fdb1a5ffa4e1774a88c0455
Drusy <kevin.renella@gmail.com>
2018-02-16 19:09:30 +0100

    v1.5 b26

--- AuvergneWebcams/UIColorExtension.swift
+++ AuvergneWebcams/UIColorExtension.swift
@@ -29,7 +29,7 @@ extension UIColor {
         var b: CGFloat = 0.0
         var a: CGFloat = 1.0
         
-        let length = hexSanitized.characters.count
+        let length = hexSanitized.count
         
         guard Scanner(string: hexSanitized).scanHexInt32(&rgb) else { return nil }
         
