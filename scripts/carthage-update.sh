991b45dc5ebdc3a222addd0e1878f86f4359f88d
Drusy <kevin.renella@gmail.com>
2018-03-07 18:05:39 +0100

    Intégrer le flux refs #26966
    Les dates sont désormais des timestamps dans les flux

--- scripts/carthage-update.sh
+++ scripts/carthage-update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-carthage update $1 --platform iOS --no-use-binaries
+carthage update $1 --platform iOS --no-use-binaries --cache-builds
 
 #find ./Carthage/Build/iOS/ -name *.bcsymbolmap $(find ./Carthage/DerivedData/Build/Products/Release-iphoneos -name *.bcsymbolmap | sed 's#./Carthage/DerivedData/Build/Products/Release-iphoneos/#-not -name #') -exec rm {} \;
